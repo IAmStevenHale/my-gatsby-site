ad7ca0b93200abaaf1166b6589b4f7bf70d0480b
iamstevenhale <iamstevehale@gmail.com>
2022-06-07 12:55:33 +1000

    Access data from Contentful and display to UI

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,24 +3,17 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `hse37015l8rp`,
+        accessToken: `3Dnwrfd2nUuTx49kiwFjLqfP8MpGSugGFvGLWNNU6Bc`,
+        host: `preview.contentful.com`,
+      },
+    },
+    "gatsby-plugin-image"
+  ]
 };
 
-plugins: [
-  {
-    resolve: `gatsby-source-contentful`,
-    options: {
-      spaceId: `hse37015l8rp`,
-      accessToken: `S5nGIqXX2dVJvtcWCaPRuXByAn4yh7hsAvt-KzkRoac`,
-      host:`https://mygatsbysitemain04357.gatsbyjs.io/`
-    },
-  },
-  {
-    resolve: `gatsby-source-contentful`,
-    options: {
-      spaceId: `hse37015l8rp`,
-      accessToken: `3Dnwrfd2nUuTx49kiwFjLqfP8MpGSugGFvGLWNNU6Bc`,
-      host: `https://preview-mygatsbysitemain04357.gtsb.io/`,
-    },
-  },
-]
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,82 @@
 import * as React from "react"
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+const Page = styled.div`
+display: grid;
+grid-template-columns: 1fr;
+grid-template-rows: 0.2fr repeat(3, 1fr);
+width: 100%;
+height: 100vh;
+`
+
+const Title = styled.h1`
+text-align: center;
+width: 100%;
+`
+
+const Blog = styled.div`
+text-align: center;
+width: 100%;
+align-content: center;
+justify-content: center;
+& p{
+  margin: 10px;
+  text-align: justify;
+}
+
+`
 
 // markup
-const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`)
+const IndexPage = ({ data }) => {
+ 
+  const pageTitle = data.allContentfulPage.edges[0].node.blocks[0].mainTitle;
+  const pageBlogText = JSON.parse(data.allContentfulPage.edges[0].node.blocks[1].blogTextField.raw).content[0].content[0].value;
   return (
     <>
-    <header>{data.site.siteMetadata.title}</header>
+    <GlobalStyle />
+    <Page>    
+      <Title>{pageTitle}</Title>
+      <Blog><p>{pageBlogText}</p></Blog>
+
+    </Page>
+
+
     </>
   )
 }
 
+export const query = graphql`
+  query HomePageQuery {
+    allContentfulPage {
+      edges {
+        node {
+          blocks {
+            ... on ContentfulBlogText {
+              id
+              blogTextField {
+                raw
+              }
+            }
+            ... on ContentfulTitle {
+              id
+              mainTitle
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  
+`
 export default IndexPage
